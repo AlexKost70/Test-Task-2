794c953fef630d6a9352dd5056485b05f2499a96
Alexander Kostrikin <aleksandrkostrikin086@gmail.com>
2023-07-22 17:44:31 +0300

    Finish first task

--- 1 task/index.js
+++ 1 task/index.js
@@ -5,7 +5,31 @@ const timerEl = document.querySelector('span');
 // Напишите реализацию createTimerAnimator
 // который будет анимировать timerEl
 const createTimerAnimator = () => {
-  return (seconds) => {};
+  const setTime = (seconds) => {
+    let hours = Math.floor(seconds / 3600);
+    let minutes = Math.floor(seconds / 60) % 60;
+    let secs = seconds % 60;
+    timerEl.innerText = `${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}:${secs.toString().padStart(2, "0")}`;
+  }
+
+  let intervalId;
+
+  return (seconds) => {
+    if (intervalId) {
+      clearInterval(intervalId);
+    }
+
+    setTime(seconds);
+
+    intervalId = setInterval(() => {
+      seconds--;
+      setTime(seconds);
+
+      if(seconds === 0) {
+        clearInterval(intervalId);
+      }
+    }, 1000)
+  };
 };
 
 const animateTimer = createTimerAnimator();
@@ -13,6 +37,7 @@ const animateTimer = createTimerAnimator();
 inputEl.addEventListener('input', () => {
   // Очистите input так, чтобы в значении
   // оставались только числа
+  inputEl.value = inputEl.value.replace(/\D/g, '');
 });
 
 buttonEl.addEventListener('click', () => {
